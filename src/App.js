a6093634f37a93f6be82842880ab757f1a7be8c6
Gayratov Javohir <javohirgayratov26@gmail.com>
2024-07-28 22:24:23 +0500

    men home pageni tugatdim

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home/Home"
 import Catalog from "./Pages/Course_catalog/Catalog"
+import Error from "./Pages/NotFound/Error"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/course_catalog" element={<Catalog/>}/>
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
